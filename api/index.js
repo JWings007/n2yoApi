e76c37992cf3d8790f01ffb620a1e8ef9ff0b863
Varun <115574133+JWings007@users.noreply.github.com>
2023-12-04 19:32:32 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const cors = require("cors");
-console.log(process.env)
 require('dotenv').config()
-let userData = {};
 
 
 const app = express();
@@ -17,13 +15,6 @@ app.listen(5000, () => {
   console.log("Server started");
 });
 
-app.post("/", (req, res) => {
-  userData = {
-    lat: req.body.lat,
-    lon: req.body.lon,
-  };
-});
-
 app.get("/", (req, res)=> {
     res.send("WELCOME TO n2yo API FETCHER")
   })
@@ -32,7 +23,7 @@ app.get("/:norad", (req, res) => {
   const id = req.params.norad;
   axios
     .get(
-      `https://api.n2yo.com/rest/v1/satellite/positions/${id}/${userData.lat}/${userData.lon}/0/300/&apiKey=${process.env.API_KEY}`
+      `https://api.n2yo.com/rest/v1/satellite/positions/${id}/12.972442/77.580643/0/300/&apiKey=${process.env.API_KEY}`
     )
     .then((response) => {
       res.send(response.data);
